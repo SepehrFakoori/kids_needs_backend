d1dcce6c43141c74aff194cc385a553ef087fed9
Mahdi <mahdi.develoop@gmail.com>
2026-02-09 23:39:35 +0330

    refactor: use env var for DB host and add .env.example

--- kids_needs_backend/settings.py
+++ kids_needs_backend/settings.py
@@ -97,10 +97,8 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': 'kids_needs_db',
+        'HOST': os.getenv('DB_HOST'),
         'PORT': '5432',
-        # 'HOST': 'localhost',
-        # 'PORT': '5432',
     }
 }
 
