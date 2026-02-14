016773f8e1dbf86e95f7431af7bb29b230492d94
Mahdi <mahdi.develoop@gmail.com>
2026-02-14 17:24:17 +0330

    feat(settings): add django-filter support to REST_FRAMEWORK

--- kids_needs_backend/settings.py
+++ kids_needs_backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
 
     # REST framework
     'rest_framework',
+    'django_filters',
 
     # OpenAPI
     'drf_spectacular',
@@ -145,6 +146,9 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
     ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_PAGINATION_CLASS': 'core.pagination.Pagination',
