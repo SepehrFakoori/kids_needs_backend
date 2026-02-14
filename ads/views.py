11b962012a73f49a3abab5de4f5a5d6e5fa52c3d
Mahdi <mahdi.develoop@gmail.com>
2026-02-14 16:51:22 +0330

    feat(ads): update permissions to allow public viewing and owner-only editing

--- ads/views.py
+++ ads/views.py
@@ -1,4 +1,4 @@
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.filters import SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
@@ -6,13 +6,14 @@ from django_filters.rest_framework import DjangoFilterBackend
 from .models import Ad
 from .serializers import AdSerializer
 from core.pagination import Pagination
+from core.permissions import IsOwnerOrReadOnly
 
 
 # Create your views here.
 class AdViewSet(ModelViewSet):
     queryset = Ad.objects.all()
     serializer_class = AdSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     pagination_class = Pagination
     filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_fields = ['creator']
