d4c6e0dddfe9118391daac425360a6ce632d475e
Sepehr <sepehrfakoori@gmail.com>
2026-02-20 11:50:38 +0330

    refactor(ads): register AdImage and Bookmark models to admin panel

--- ads/admin.py
+++ ads/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Ad
+from .models import Ad, AdImage, Bookmark
 
 # Register your models here.
 admin.site.register(Ad)
+admin.site.register(AdImage)
+admin.site.register(Bookmark)
